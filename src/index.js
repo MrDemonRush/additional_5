7654b1473c6f91c628a0f8ac205e4f0b626d590e
MDR <mrdemonrush@gmail.com>
2018-09-30 17:32:53 +0300

    Correct_expression v.1.0.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const arrStr = str.split("");
+  const arrStrNew = [];
+  const strLength = str.length;
+  const configLength = bracketsConfig.length;
+
+  for (let i = 0; i < strLength; i++) {
+
+    for (let j = 0; j < configLength; j++) {
+      
+      if (bracketsConfig[j][0] == arrStr[i] && bracketsConfig[j][1] == arrStr[i + 1]) {
+        arrStrNew.push(arrStr.splice(i, 2));
+        i = -1;
+      }
+    }
+  }
+  
+  return arrStrNew.length * 2 == strLength ? true : false;
 }
